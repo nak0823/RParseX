ed9746c15f3de89a99969ea8853baec336261eef
Nakateru <130221150+nak0823@users.noreply.github.com>
2023-08-02 15:00:25 +0200

    Moved some functions 💥

--- RParseX.cpp
+++ RParseX.cpp
@@ -6,32 +6,13 @@
 
 std::vector<std::string> GetDirectoryPaths(std::string);
 void Scraper(std::string);
-void ConsoleTitle(const std::string& title);
-
-bool DirectoryExists(const std::string& path) {
-	DWORD attribute = GetFileAttributesA(path.c_str());
-	return (attribute != INVALID_FILE_ATTRIBUTES && (attribute & FILE_ATTRIBUTE_DIRECTORY));
-}
-
-std::string GetLogsLocation() {
-	std::string logsPath;
-
-	while (true) {
-		std::cout << "Enter the location of the logs: ";
-		std::getline(std::cin, logsPath);
-
-		if (!DirectoryExists(logsPath)) {
-			system("cls");
-			GetLogsLocation();
-		}
-
-		return logsPath;
-	}
-}
+void SetConsoleTitle(const std::string& title);
+bool DirectoryExists(const std::string& path);
+std::string GetLogsLocation();
 
 int main()
 {
-	ConsoleTitle("RParseX 1.0.0 ~ by Serialized");
+	SetConsoleTitle("RParseX 1.0.0 ~ by Serialized");
 	std::string logsPath = GetLogsLocation();
 	Scraper(logsPath);
 }
